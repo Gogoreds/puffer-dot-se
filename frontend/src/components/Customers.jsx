cdc5358772791f1798751005957ddb240d3cff2a
Mohammad Arefi <m-arefi@live.com>
2022-12-21 18:28:55 +0100

    Logos added

--- frontend/src/components/Customers.jsx
+++ frontend/src/components/Customers.jsx
@@ -9,8 +9,8 @@ import logoTuple from "@/images/logos/tuple.svg";
 
 export function Customers() {
   return (
-    <div className="mt-32 bg-[#212240]">
-      <p className="text-center text-2xl font-bold tracking-tight text-white sm:text-4xl">
+    <div className="bg-[#212240] py-32">
+      <p className="pb-8 text-center text-2xl font-bold tracking-tight text-white sm:text-4xl">
         Customers who cannot just get enough
       </p>
       <ul
@@ -32,7 +32,27 @@ export function Customers() {
           <li key={groupIndex}>
             <ul
               role="list"
-              className="flex flex-col items-center gap-y-8 sm:flex-row sm:gap-x-12 sm:gap-y-0"
+              className="flex flex-col items-center gap-y-8 py-4 sm:flex-row sm:gap-x-12 sm:gap-y-0"
+            >
+              {group.map((company) => (
+                <li key={company.name} className="flex">
+                  <Image src={company.logo} alt={company.name} unoptimized />
+                </li>
+              ))}
+            </ul>
+            <ul
+              role="list"
+              className="flex flex-col items-center gap-y-8 py-4 sm:flex-row sm:gap-x-12 sm:gap-y-0"
+            >
+              {group.map((company) => (
+                <li key={company.name} className="flex">
+                  <Image src={company.logo} alt={company.name} unoptimized />
+                </li>
+              ))}
+            </ul>
+            <ul
+              role="list"
+              className="flex flex-col items-center gap-y-8 py-4 sm:flex-row sm:gap-x-12 sm:gap-y-0"
             >
               {group.map((company) => (
                 <li key={company.name} className="flex">
