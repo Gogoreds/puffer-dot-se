d1c46deac3b34fd759ba9f212459bcef47a512c3
Mohammad Arefi <m-arefi@live.com>
2022-12-22 15:31:48 +0100

    Hero gradient setup

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -5,7 +5,7 @@ import SEOBanner from "../images/SEOHero.png";
 
 export function Hero() {
   return (
-    <div className="w-full bg-[#212240] pt-48">
+    <div className="w-full bg-gradient-to-tr from-[#212240] via-[#212240]  to-indigo-400 pt-60">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="overflow-visible bg-transparent lg:grid lg:grid-cols-2 lg:gap-4">
           <div className="px-6 pt-10 pb-12 sm:px-16 sm:pt-16 lg:py-16 lg:pr-0 xl:py-20 xl:px-20">
