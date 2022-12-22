09fc11a7364db922fad7921017d92de83fba2509
Mohammad Arefi <m-arefi@live.com>
2022-12-22 15:51:35 +0100

    Login/Register init

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,69 +1,52 @@
-import Head from "next/head";
-import Link from "next/link";
+import Head from 'next/head'
+import Link from 'next/link'
 
-import { AuthLayout } from "@/components/AuthLayout";
-import { Button } from "@/components/Button";
-import { TextField } from "@/components/Fields";
-import { Logo } from "@/components/Logo";
+import { AuthLayout } from '@/components/AuthLayout'
+import { Button } from '@/components/Button'
+import { TextField } from '@/components/Fields'
 
 export default function Login() {
   return (
     <>
       <Head>
-        <title>Sign In - TaxPal</title>
+        <title>Sign In - Pocket</title>
       </Head>
-      <AuthLayout>
-        <div className="flex flex-col">
-          <Link href="/" aria-label="Home">
-            <Logo className="h-10 w-auto" />
-          </Link>
-          <div className="mt-20">
-            <h2 className="text-lg font-semibold text-gray-900">
-              Sign in to your account
-            </h2>
-            <p className="mt-2 text-sm text-gray-700">
-              Don’t have an account?{" "}
-              <Link
-                href="/register"
-                className="font-medium text-blue-600 hover:underline"
-              >
-                Sign up
-              </Link>{" "}
-              for a free trial.
-            </p>
+      <AuthLayout
+        title="Sign in to account"
+        subtitle={
+          <>
+            Don’t have an account?{' '}
+            <Link href="/register" className="text-cyan-600">
+              Sign up
+            </Link>{' '}
+            for a free trial.
+          </>
+        }
+      >
+        <form>
+          <div className="space-y-6">
+            <TextField
+              label="Email address"
+              id="email"
+              name="email"
+              type="email"
+              autoComplete="email"
+              required
+            />
+            <TextField
+              label="Password"
+              id="password"
+              name="password"
+              type="password"
+              autoComplete="current-password"
+              required
+            />
           </div>
-        </div>
-        <form action="#" className="mt-10 grid grid-cols-1 gap-y-8">
-          <TextField
-            label="Email address"
-            id="email"
-            name="email"
-            type="email"
-            autoComplete="email"
-            required
-          />
-          <TextField
-            label="Password"
-            id="password"
-            name="password"
-            type="password"
-            autoComplete="current-password"
-            required
-          />
-          <div>
-            <Button
-              type="submit"
-              variant="solid"
-              color="blue"
-              className="w-full"
-            >
-              <span>
-                Sign in <span aria-hidden="true">&rarr;</span>
-              </span>
-            </Button>
-          </div>
+          <Button type="submit" color="cyan" className="mt-8 w-full">
+            Sign in to account
+          </Button>
         </form>
       </AuthLayout>
     </>
-  );
+  )
 }
--- frontend/src/pages/register.jsx
+++ frontend/src/pages/register.jsx
@@ -1,101 +1,81 @@
-import Head from "next/head";
-import Link from "next/link";
+import Head from 'next/head'
+import Link from 'next/link'
 
-import { AuthLayout } from "@/components/AuthLayout";
-import { Button } from "@/components/Button";
-import { SelectField, TextField } from "@/components/Fields";
-import { Logo } from "@/components/Logo";
+import { AuthLayout } from '@/components/AuthLayout'
+import { Button } from '@/components/Button'
+import { SelectField, TextField } from '@/components/Fields'
 
 export default function Register() {
   return (
     <>
       <Head>
-        <title>Sign Up - TaxPal</title>
+        <title>Sign Up - Pocket</title>
       </Head>
-      <AuthLayout>
-        <div className="flex flex-col">
-          <Link href="/" aria-label="Home">
-            <Logo className="h-10 w-auto" />
-          </Link>
-          <div className="mt-20">
-            <h2 className="text-lg font-semibold text-gray-900">
-              Get started for free
-            </h2>
-            <p className="mt-2 text-sm text-gray-700">
-              Already registered?{" "}
-              <Link
-                href="/login"
-                className="font-medium text-blue-600 hover:underline"
-              >
-                Sign in
-              </Link>{" "}
-              to your account.
-            </p>
-          </div>
-        </div>
-        <form
-          action="#"
-          className="mt-10 grid grid-cols-1 gap-y-8 gap-x-6 sm:grid-cols-2"
-        >
-          <TextField
-            label="First name"
-            id="first_name"
-            name="first_name"
-            type="text"
-            autoComplete="given-name"
-            required
-          />
-          <TextField
-            label="Last name"
-            id="last_name"
-            name="last_name"
-            type="text"
-            autoComplete="family-name"
-            required
-          />
-          <TextField
-            className="col-span-full"
-            label="Email address"
-            id="email"
-            name="email"
-            type="email"
-            autoComplete="email"
-            required
-          />
-          <TextField
-            className="col-span-full"
-            label="Password"
-            id="password"
-            name="password"
-            type="password"
-            autoComplete="new-password"
-            required
-          />
-          <SelectField
-            className="col-span-full"
-            label="How did you hear about us?"
-            id="referral_source"
-            name="referral_source"
-          >
-            <option>AltaVista search</option>
-            <option>Super Bowl commercial</option>
-            <option>Our route 34 city bus ad</option>
-            <option>The “Never Use This” podcast</option>
-          </SelectField>
-          <div className="col-span-full">
-            <Button
-              type="submit"
-              variant="solid"
-              color="blue"
-              className="w-full"
+      <AuthLayout
+        title="Sign up for an account"
+        subtitle={
+          <>
+            Already registered?{' '}
+            <Link href="/login" className="text-cyan-600">
+              Sign in
+            </Link>{' '}
+            to your account.
+          </>
+        }
+      >
+        <form>
+          <div className="grid grid-cols-2 gap-6">
+            <TextField
+              label="First name"
+              id="first_name"
+              name="first_name"
+              type="text"
+              autoComplete="given-name"
+              required
+            />
+            <TextField
+              label="Last name"
+              id="last_name"
+              name="last_name"
+              type="text"
+              autoComplete="family-name"
+              required
+            />
+            <TextField
+              className="col-span-full"
+              label="Email address"
+              id="email"
+              name="email"
+              type="email"
+              autoComplete="email"
+              required
+            />
+            <TextField
+              className="col-span-full"
+              label="Password"
+              id="password"
+              name="password"
+              type="password"
+              autoComplete="new-password"
+              required
+            />
+            <SelectField
+              className="col-span-full"
+              label="How did you hear about us?"
+              id="referral-source"
+              name="referral_source"
             >
-              <span>
-                Sign up <span aria-hidden="true">&rarr;</span>
-              </span>
-            </Button>
+              <option>AltaVista search</option>
+              <option>Super Bowl commercial</option>
+              <option>Our route 34 city bus ad</option>
+              <option>The “Never Use This” podcast</option>
+            </SelectField>
           </div>
+          <Button type="submit" color="cyan" className="mt-8 w-full">
+            Get started today
+          </Button>
         </form>
       </AuthLayout>
     </>
-  );
+  )
 }
